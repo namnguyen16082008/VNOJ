5df99435cb579b44b9158e1c86f9aab3b1b4782e
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-03-27 21:33:54 +0700

    https://oj.vnoi.info/problem/fc134_maxmod

---
+++ fc134_maxmod.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+ll t,n;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>t;
+    while (t--){
+        cin>>n;
+        cout<<n/2+1<<endl;
+    }
+}
