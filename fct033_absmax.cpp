de661c2134f7b62c103fa01878d39fe179183cc6
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-04-14 19:37:54 +0700

    https://oj.vnoi.info/problem/fct033_absmax

---
+++ fct033_absmax.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+const ll N=200005;
+ll n,a[N];
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>n;
+    for (ll i=1;i<=n;i++) cin>>a[i];
+    sort(a+1,a+n+1);
+    cout<<max(abs(a[1]+a[2]),abs(a[n]+a[n-1]));
+}
