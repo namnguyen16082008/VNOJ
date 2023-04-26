9c38e8db4641f87cc7e1623480a449547af36a69
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-04-26 22:48:13 +0700

    https://oj.vnoi.info/problem/fcb041_onedivk

---
+++ fcb041_onedivk.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+ll n,s=0;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>n;
+    for (ll i=1;i<=n;i++){
+        s=(s*10+1)%n;
+        if (s==0){
+            cout<<i;
+            return 0;
+        }
+    }
+}
