d30fe476620f4089c19fde74d04fb40e5678b96d
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-04-27 20:35:21 +0700

    https://oj.vnoi.info/problem/fc100_fc100

---
+++ fc100_fc100.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+string s;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    while (getline(cin,s)){
+        if (s=="Free Contest 100"){
+            cout<<"Let's start!"<<endl;
+            return 0;
+        }
+        else cout<<"Wait"<<endl;
+    }
+}
