9422a47408b7791b33eb93f12bbd89b6bfb5cd24
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-05-08 18:37:14 +0700

    https://oj.vnoi.info/problem/bedao_m19_line

---
+++ bedao_m19_line.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define ll long long int
+
+using namespace std;
+
+ll n;
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>n;
+    cout<<0<<" "<<0<<endl<<99<<" "<<99<<endl<<99<<" "<<0<<endl<<0<<" "<<99<<endl;
+    for (ll i=1;i<=n-4;i++) cout<<i<<" "<<i<<endl;
+    cout<<0<<" "<<0<<" "<<99<<" "<<99<<endl<<99<<" "<<0<<" "<<0<<" "<<99;
+}
+
