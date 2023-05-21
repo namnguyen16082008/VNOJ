f6a2e652bc8b884b4bdb6a9c13e0a9691a2a5a65
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-05-21 16:09:39 +0700

    https://oj.vnoi.info/problem/fc147_eventuples

--- fc147_eventuples.cpp
+++ fc147_eventuples.cpp
@@ -9,44 +9,24 @@ struct Node{
     ll Chan,Le;
 };
 Node ST[N];
-void Build(ll id, ll l, ll r){
-    if (l==r){
-        if (a[l]%2==0) ST[id].Chan++;
-        else ST[id].Le++;
-        return;
+void Build(){
+    ST[0].Chan=0;
+    ST[0].Le=0;
+    for (ll i=1;i<=n;i++){
+        ST[i].Chan=ST[i-1].Chan+(a[i]%2==0);
+        ST[i].Le=ST[i-1].Le+(a[i]%2==1);
     }
-    ll md=(l+r)/2;
-    Build(id*2,l,md);
-    Build(id*2+1,md+1,r);
-    ST[id].Chan=ST[id*2].Chan+ST[id*2+1].Chan;
-    ST[id].Le=ST[id*2].Le+ST[id*2+1].Le;
-}
-ll Get_Chan(ll id, ll l, ll r, ll x, ll y){
-    if (l>y || r<x) return 0;
-    if (l>=x && r<=y) return ST[id].Chan;
-    ll md=(l+r)/2;
-    return Get_Chan(id*2,l,md,x,y)+Get_Chan(id*2+1,md+1,r,x,y);
-}
-ll Get_Le(ll id, ll l, ll r, ll x, ll y){
-    if (l>y || r<x) return 0;
-    if (l>=x && r<=y) return ST[id].Le;
-    ll md=(l+r)/2;
-    return Get_Le(id*2,l,md,x,y)+Get_Le(id*2+1,md+1,r,x,y);
 }
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cin>>n>>q;
-    for (ll i=1;i<=N;i++){
-        ST[i].Chan=0;
-        ST[i].Le=0;
-    }
     for (ll i=1;i<=n;i++) cin>>a[i];
-    Build(1,1,n);
+    Build();
     while (q--){
         cin>>x>>y;
-        c=Get_Chan(1,1,n,x,y);
-        l=Get_Le(1,1,n,x,y);
+        c=ST[y].Chan-ST[x-1].Chan;
+        l=ST[y].Le-ST[x-1].Le;
         cout<<c*(c-1)*(c-2)/6+l*(l-1)*c/2<<endl;
     }
 }
