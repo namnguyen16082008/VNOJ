716850f24dab71f5429141db920a91067b84381b
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-06-26 15:21:55 +0700

    https://oj.vnoi.info/problem/fct006_lcmlimit

--- fct006_lcmlimit.cpp
+++ fct006_lcmlimit.cpp
@@ -4,15 +4,16 @@
 using namespace std;
 
 ll n,s=510510;
+vector <ll> v;
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cin>>n;
-    for (ll i=1;i<=s;i++)
+    for (ll i=1;i<=sqrt(s);i++)
         if (s%i==0){
-            cout<<i<<" ";
-            n--;
-            if (!n) break;
+            v.push_back(i);
+            if (i*i!=s) v.push_back(s/i);
         }
+    for (ll i=0;i<n;i++) cout<<v[i]<<" ";
 }
 
