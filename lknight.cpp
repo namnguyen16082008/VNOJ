0a801adcf0ec13d28cf97882205def9b1146179a
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-07-03 00:30:30 +0700

    https://oj.vnoi.info/problem/lknight

--- lknight.cpp
+++ lknight.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-ll n,k,o,x=0,y=0,a=0,b=0,c=0,d=0;
+ll n,k,o,x=0,y=0,a=0,b=0,c=0,d=0,fi[8]={1,2,2,1,-1,-2,-2,-1},fj[8]={2,1,-1,-2,-2,-1,1,2};
 char o1;
 int main(){
     ios_base::sync_with_stdio(false);
@@ -12,14 +12,10 @@ int main(){
     for (ll i=1;i<=k;i++){
         cin>>o1;
         o=int(o1)-48;
-        if (o<=2){c+=o;d+=3-o;}
-        else if (o<=4){c+=5-o;d+=2-o;}
-        else if (o<=6){c+=4-o;d+=o-7;}
-        else if (o<=8){c+=o-9;d+=o-6;}
-        x=min(x,c);
-        y=min(y,d);
-        a=max(a,c);
-        b=max(b,d);
+        c+=fi[o-1];
+        d+=fj[o-1];
+        x=min(x,c);y=min(y,d);
+        a=max(a,c);b=max(b,d);
     }
     cout<<(n-a+x)*(n-b+y);
 }
