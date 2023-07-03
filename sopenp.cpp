7f6dad564352101a9a1f82ae730576e0e19cbda8
Nam Nguyễn <128302088+namnguyen16082008@users.noreply.github.com>
2023-07-03 17:54:14 +0700

    https://oj.vnoi.info/problem/sopenp

--- sopenp.cpp
+++ sopenp.cpp
@@ -4,42 +4,29 @@
 using namespace std;
 
 const ll N=1048576;
-ll n,l,u,ans=0,a[N+5],x[N+5],y[N+5];
-void Build1(){
-    ll d=1,m=0;
-    unordered_map <ll,ll> mp;
-    for (ll i=1;i<=n;i++){
-        m+=mp[a[i]]==0;
-        mp[a[i]]++;
-        while (m>l-1){
-            mp[a[d]]--;
-            m-=mp[a[d]]==0;
-            d++;
-        }
-        x[i]=i-d+1;
-    }
-}
-void Build2(){
-    ll d=1,m=0;
-    unordered_map <ll,ll> mp;
-    for (ll i=1;i<=n;i++){
-        m+=mp[a[i]]==0;
-        mp[a[i]]++;
-        while (m>u){
-            mp[a[d]]--;
-            m-=mp[a[d]]==0;
-            d++;
-        }
-        y[i]=i-d+1;
-    }
-}
+ll n,l,u,d1=1,d2=1,m1=0,m2=0,ans=0,a[N+5],x[N+5],y[N+5];
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cin>>n>>l>>u;
     for (ll i=1;i<=n;i++) cin>>a[i];
-    Build1();
-    Build2();
-    for (ll i=1;i<=n;i++) ans+=y[i]-x[i];
+    unordered_map <ll,ll> mp1,mp2;
+    for (ll i=1;i<=n;i++){
+        m1+=mp1[a[i]]==0;
+        m2+=mp2[a[i]]==0;
+        mp1[a[i]]++;
+        mp2[a[i]]++;
+        while (m1>u){
+            mp1[a[d1]]--;
+            m1-=mp1[a[d1]]==0;
+            d1++;
+        }
+        while (m2>l-1){
+            mp2[a[d2]]--;
+            m2-=mp2[a[d2]]==0;
+            d2++;
+        }
+        ans+=d2-d1;
+    }
     cout<<ans;
 }
